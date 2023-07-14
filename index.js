f15c9097069f7b6ec1147a3a103e1c8b66e316b7
Jeroen Engels <jfm.engels@gmail.com>
2023-07-14 23:26:58 +0200

    Slice before recursive call

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function help(str, i, reStr, inGroup, isAtTheStartOfSegment) {
       }
     } else {
       // it's not a globstar, so only match one path segment
-      return help(newStr, 1, reStr + "([^/]*)", inGroup, false);
+      return help(newStr.slice(1), 0, reStr + "([^/]*)", inGroup, false);
     }
 
   default:
